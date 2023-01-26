2bc6c8400c2c7185ecb2f9bf366d44c477806058
jedrek <olkowski.jedrzej@gmail.com>
2023-01-27 00:14:29 +0100

    selecting common years

--- src/analiza/main.py
+++ src/analiza/main.py
@@ -17,6 +17,9 @@ data_emisjaCO2 = pd.read_csv(args.CO2emission)
 from_year = args.f
 to_year = args.t
 
+data_PKB.drop('Unnamed: 66', axis=1, inplace=True)
+data_mieszkancy.drop('Unnamed: 66', axis=1, inplace=True)
+
 #zamienam nazwy krajow we wszystkich danych na pisane tylko duzymi literami
 data_PKB['Country Name'] = data_PKB['Country Name'].str.upper()
 data_mieszkancy['Country Name'] = data_mieszkancy['Country Name'].str.upper()
@@ -35,19 +38,21 @@ data_emisjaCO2['Country'].replace(corec_dict, inplace=True)
 diff_names = (set(data_PKB['Country Name']) | (set(data_mieszkancy['Country Name'])) | (set(data_emisjaCO2['Country'])) ) - \
        (set(data_PKB['Country Name']) & set(data_mieszkancy['Country Name']) & set(data_emisjaCO2['Country']) )
 
-#print( list( sorted(diff_names) ) )
 #pozbywam sie krajow niewystepujacych we wszsytkich 3 zbiorach danych
-print( data_PKB.shape )
-print( data_mieszkancy.shape )
-print( data_emisjaCO2.shape )
-
 data_PKB = data_PKB[~data_PKB['Country Name'].isin( diff_names)]
 data_mieszkancy = data_mieszkancy[~data_mieszkancy['Country Name'].isin(diff_names)]
 data_emisjaCO2 = data_emisjaCO2[~data_emisjaCO2['Country'].isin(diff_names)]
 
+#tworze zbior lat wystepujacych we wszystkich zbiorach i z przedzialu [from_year, to_year]
+com_years = sorted( set( np.arange(from_year, to_year, 1, dtype='int').astype(str) ) & set(data_PKB.columns) \
+            & set(data_mieszkancy.columns) & set(data_emisjaCO2['Year'].astype(str)) )
+
+#scalam dane ze wzgledu na rok a potem ze wzgledu na nazwe kraju
+
 
 print( data_PKB.shape )
 print( data_mieszkancy.shape )
 print( data_emisjaCO2.shape )
-print( from_year )
-print( to_year )
+print( data_PKB.head )
+print( data_mieszkancy.head )
+print( data_emisjaCO2.head )
