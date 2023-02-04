da9831fb12d001921301657f845cf0fb8a700ccd
jedrek <olkowski.jedrzej@gmail.com>
2023-02-04 01:55:21 +0100

    modules

---
+++ src/analiza/__init__.py
@@ -0,0 +1,4 @@
+from funkcje_analizy_danych import CO2_na_mieszkanca, GDP_na_mieszkanca, zmiana_emisji
+from wczytaj import wczytaj_formatuj
+
+__all__ = ("CO2_na_mieszkanca", "GDP_na_mieszkanca", "zmiana_emisji", "wczytaj_formatuj")
