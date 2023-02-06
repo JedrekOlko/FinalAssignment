c274cf7397cfddde9d92676d62cefaf037b54709
jedrek <olkowski.jedrzej@gmail.com>
2023-02-06 17:50:43 +0100

    polishing package

--- src/analiza/wczytaj.py
+++ src/analiza/wczytaj.py
@@ -1,23 +1,7 @@
-import argparse
 import pandas as pd
 import numpy as np
-import pickle
 
-def wczytaj_formatuj():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('gdp', help='path to gdp csv')
-    parser.add_argument('population', help='path to population csv')
-    parser.add_argument('CO2emission', help='path to CO2 emission csv')
-    parser.add_argument('-f', type=int, help='from which year', default=1961)
-    parser.add_argument('-t', type=int, help='to which year', default=2014)
-    args = parser.parse_args()
-
-    data_PKB = pd.read_csv(args.gdp, skiprows=4)
-    data_mieszkancy = pd.read_csv(args.population, skiprows=3)
-    data_emisjaCO2 = pd.read_csv(args.CO2emission)
-    from_year = args.f
-    to_year = args.t
-
+def wczytaj_formatuj(data_PKB, data_mieszkancy, data_emisjaCO2, from_year, to_year):
     data_PKB.drop('Unnamed: 66', axis=1, inplace=True)
     data_mieszkancy.drop('Unnamed: 66', axis=1, inplace=True)
 
